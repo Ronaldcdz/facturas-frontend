ea82f4c2ebd2e8e0fd2a1d35fd0a85b08aab0feb
RonaldLug <ronald.cadiz@lugotech.com.do>
2026-04-27 09:00:45 -0400

    remove: no existing import

--- app/clientes/_components/ClientCard.tsx
+++ app/clientes/_components/ClientCard.tsx
@@ -1,26 +1,19 @@
 import { Cliente } from "@/components/features/clientes/schema";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatDocument } from "@/lib/utils";
 import { MapPin, Ticket } from "lucide-react";
-import ClientCardActions from "./ClientCardActions";
 import { ReactNode } from "react";
 
 type Props = {
   cliente: Cliente;
-  children?: ReactNode
+  children?: ReactNode;
 };
 
-export default function ClientCard({
-  cliente, children }: Props) {
-
-  const direccion: string = cliente.ciudad && Object.keys(cliente.ciudad).length > 0
-    ? `${cliente.ciudad.nombre}, ${cliente.ciudad.provincia}`
-    : "N/A";
+export default function ClientCard({ cliente, children }: Props) {
+  const direccion: string =
+    cliente.ciudad && Object.keys(cliente.ciudad).length > 0
+      ? `${cliente.ciudad.nombre}, ${cliente.ciudad.provincia}`
+      : "N/A";
 
   return (
     <Card className="flex flex-row justify-between gap-4 px-6 py-2 items-center bg-white">
@@ -30,7 +23,9 @@ export default function ClientCard({
       <CardHeader className="w-full px-0 flex">
         <CardTitle className="flex gap-1 flex-col self-center">
           <span className="text-xl">{cliente.nombre}</span>
-          <span className="text-md opacity-50">RNC: {formatDocument(cliente.rnc)}</span>
+          <span className="text-md opacity-50">
+            RNC: {formatDocument(cliente.rnc)}
+          </span>
           <span className="text-md opacity-50 flex gap-1">
             <span>
               <MapPin className="h-5 w-5" />
@@ -39,7 +34,6 @@ export default function ClientCard({
           </span>
         </CardTitle>
         <CardContent className="flex flex-col gap-4 ml-auto">
-          {/* <ClientCardActions cliente={cliente} /> */}
           {children}
         </CardContent>
       </CardHeader>
